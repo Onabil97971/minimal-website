0c048cdcafd5d0c012388952d4ed70bb1f36f0b7
Onabil97971 <ONO97971@yahoo.com>
2022-11-17 01:24:55 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Intro from "./Components/Intro";
+import Portfolio from "./Components/Portfolio";
+import Timeline from "./Components/Timeline";
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+        Omar Nabil Omar
+      </h1>
+      <p>Software Engineer & Web Developer</p>
+      <p>This is my bio</p>
+    </div>
+  );
+}
+
+export default Intro;
