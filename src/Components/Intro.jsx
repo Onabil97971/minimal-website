73d020ce9c1d17b54d691c26ce37f08bbd447174
Onabil97971 <ONO97971@yahoo.com>
2022-11-18 23:56:20 +0200

    add feature for intro

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,11 +3,17 @@ import React from "react";
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold text-cyan-600">
         Omar Nabil Omar
       </h1>
-      <p>Software Engineer & Web Developer</p>
-      <p>This is my bio</p>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Software Engineer & Web Developer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Im a Self-Taught Programmer based in Port Said, Egypt,I have beein
+        Coding sicne 13 years old and my first project was a simple calculator
+        made with visual basic
+      </p>
     </div>
   );
 }
